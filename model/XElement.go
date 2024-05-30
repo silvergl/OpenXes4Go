ad5bf90add248a5d730f286cc49568c4ff7f22a6
Serafim Simonov <stu126367@mail.uni-kiel.de>
2024-05-30 13:14:58 +0200

    Implement custom marshal for XLog

--- model/XElement.go
+++ model/XElement.go
@@ -39,6 +39,62 @@ type XLog struct {
 	classifiers []XClassifier
 }
 
+// MarshalXML customizes the XML marshaling for the Person struct
+func (x XLog) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	type xlog XLog
+	start.Name = xml.Name{Local: "log"}
+	start.Attr = []xml.Attr{
+		{Name: xml.Name{Local: "xes.version"}, Value: x.Version},
+		{Name: xml.Name{Local: "xes.features"}, Value: x.Features},
+		{Name: xml.Name{Local: "openxes.version"}, Value: x.OpenXesVersion},
+		{Name: xml.Name{Local: "xes.version"}, Value: x.Xmlns},
+	}
+
+	// Create the starting element
+	if err := e.EncodeToken(start); err != nil {
+		return err
+	}
+	startElement := xml.StartElement{Name: xml.Name{Local: "Global"}}
+	startElement.Attr = []xml.Attr{{Name: xml.Name{Local: "scope"}, Value: "trace"}}
+
+	if err := e.EncodeToken(startElement); err != nil {
+		return err
+	}
+	// Marshal the MaleFriends with gender attribute
+	for _, globalEvent := range x.GlobalEventAttributes {
+
+		if err := e.EncodeElement(globalEvent.Key, xml.StartElement{Name: xml.Name{Local: "name"}}); err != nil {
+			return err
+		}
+
+		if err := e.EncodeToken(xml.EndElement{Name: startElement.Name}); err != nil {
+			return err
+		}
+	}
+	eventElement := xml.StartElement{Name: xml.Name{Local: "Global"}}
+	eventElement.Attr = []xml.Attr{{Name: xml.Name{Local: "scope"}, Value: "event"}}
+	if err := e.EncodeToken(eventElement); err != nil {
+		return err
+	}
+	// Marshal the FemaleFriends with gender attribute
+	for _, globalEvent := range x.GlobalTraceAttributes {
+
+		if err := e.EncodeToken(startElement); err != nil {
+			return err
+		}
+		if err := e.EncodeElement(globalEvent.Key, xml.StartElement{Name: xml.Name{Local: "name"}}); err != nil {
+			return err
+		}
+
+		if err := e.EncodeToken(xml.EndElement{Name: startElement.Name}); err != nil {
+			return err
+		}
+	}
+
+	// End the Person element
+	return e.EncodeToken(xml.EndElement{Name: start.Name})
+}
+
 type Global struct {
 	Scope string `xml:"scope,attr"`
 	attributes2.XAttribute
